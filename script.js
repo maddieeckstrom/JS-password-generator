9728cabc8542a9a07c45b2ccc205433b9e734c3e
Maddie <maddieeckstrom@gmail.com>
2023-08-23 08:59:53 -1000

    added prompt for password length criteria and the skeleton framework of confirm criteria

--- script.js
+++ script.js
@@ -1,5 +1,60 @@
-// Assignment Code
 var generateBtn = document.querySelector("#generate");
+
+function generatePassword() {
+  let passwordCriteria = {
+    min: 8,
+    max: 128
+  };
+ 
+  let passwordLength = prompt("How long would you like your password to be?");
+  console.log(passwordLength);
+
+  if (passwordLength < passwordCriteria.min) {
+    alert("Password must be minimum of 8 characters")
+    return
+  };
+  if (passwordLength > passwordCriteria.max) {
+    alert("Password must be maximum of 128 characters")
+    return
+  };
+
+  let passwordNumbers = "0123456789";
+  let passwordLowercase = "abcdefghijklmnopqrstuvwxyz";
+  let passwordUppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  let passwordCharacters = " !#$%&'()*+,-./:;<=>?@[\]^_`{|}~:";
+
+// confirm commands and if else commands go here
+
+  let numbersTag = confirm("Would you like to include numbers in your password?");
+  if (numbersTag === true) {
+// how do I validate this here?
+  };
+
+  let lowercaseTag = confirm("Would you like to include lowercase letters in your password?");
+  if (lowercaseTag === true) {
+
+  };
+
+  let uppercaseTag = confirm("Would you like to include uppercase letters in your password?");
+  if (uppercaseTag === true) {
+
+  };
+
+  let charactersTag = confirm("Would you like to include special characters in your password?");
+  if (charactersTag === true) {
+
+  };
+
+  if(numbersTag !== true && lowercaseTag !== true && uppercaseTag !== true && charactersTag !== true) {
+    alert("You must choose to include one of four options to create a valid password");
+  }
+
+  
+  // for (var i = 0; i = passwordLength; i++) {
+  // };
+  
+  return 'newPassword';
+}
 
 // Write password to the #password input
 function writePassword() {
@@ -10,5 +65,7 @@ function writePassword() {
 
 }
 
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
